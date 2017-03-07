4215ace346acf2ca2167bc9f26e050599dd5e4f1
canfeit <canfeit@qq.com>
2017-03-07 15:50:01 +0800

    u

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,8 @@
-mv -f /opt/node/ /opt/node$(date +%Y%m%d)
-unzip /opt/node.zip -d /opt/node
+pm2 stop all
+rm -rf /opt/js-node$(date +%Y%m%d)
+mv -f /opt/node /opt/js-node$(date +%Y%m%d)
+unzip -q /opt/node.zip -d /opt/node
+mv -f /opt/node*/ /opt/node
+chmod -R 655 /opt/node
+node -v
+pm2 start all
