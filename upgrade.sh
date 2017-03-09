cb89241001a77f7b4e183f571c607cb7b31556db
canfeit <canfeit@qq.com>
2017-03-09 22:51:59 +0800

    u

--- upgrade.sh
+++ upgrade.sh
@@ -1,12 +1,20 @@
+cd ~
 pm2 stop all
 rm -rf /opt/js-node$(date +%Y%m%d)
-mv -f /opt/node /opt/js-node$(date +%Y%m%d)
-unzip -q /opt/node.zip -d /opt/node
-mv -f /opt/node*/ /opt/node
-cp -R /opt/js-node$(date +%Y%m%d)/bin/npm /opt/node/bin/
+cp -r /opt/node /opt/js-node$(date +%Y%m%d)
+unzip -q /opt/node.zip -d /opt/tmp-node
+rm -rf /opt/node/include /opt/node/share /opt/node/bin
+mv /opt/tmp-node/bin /opt/node
+mv /opt/tmp-node/include /opt/node
+mv /opt/tmp-node/share /opt/node
+rm -rf /opt/tmp-node /opt/node/bin/npm
+cd /opt/node/bin
+ln -s ../lib/node_modules/npm/bin/npm-cli.js npm
 chmod -R 655 /opt/node
 /opt/node/bin/node -v
 node -v
-npm i -g pm2@latest
+/opt/node/bin/npm -v
+npm i -g npm
+npm -v
 pm2 update
 pm2 restart all --update-env
